8e93328e3fc50a453bc2898ee21cc343a75289f5
bubbles <152947339+KernFerm@users.noreply.github.com>
2024-01-07 22:30:57 -0500

    Add files via upload

---
+++ PlayPingPong.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Set the path to your Python interpreter
+set PYTHOPATH=C:\Python310
+
+REM Set the path to your script
+set SCRIPTPATH=C:\Users\Username\Documents\Python\Pong.py
+
+REM Run the script
+%PYTHOPATH%\python.exe %SCRIPTPATH%
